a6229048a870fad66a7ce0208188ebaf306ce63c
Deadman407 <dead.407.man@gmail.com>
2021-12-18 06:39:38 -0800

    Added Vuex Store

--- cart/views.py
+++ cart/views.py
@@ -4,8 +4,16 @@ from .cart import Cart
 
 def cart_detail(request):
     cart = Cart(request)
+    productstring = ''
+
+    for item in cart:
+        product = item['product']
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'])
+
+        productstring = productstring + b
 
     context = {
-        'cart': cart
+        'cart': cart,
+        'productstring': productstring
     }
     return render(request, 'cart.html', context)
